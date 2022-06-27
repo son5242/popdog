2fbb1791f2af3539bf18c401e07508458e477d27
son5242 <98200208+son5242@users.noreply.github.com>
2022-06-27 20:47:17 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
-const a=document.querySelector("#popdog")
+const a=document.querySelector("#popdog");
+const score = document.querySelector("#score");
+let j=0;
+const sound = new Audio("pop1.mp3");
 function change1(){
-    a.src="pop2.png"
+    
+    a.src = "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FdcoqqD%2FbtrFKaCMe71%2F8xcgd9ltnhJO9v7GVugsdk%2Fimg.png";
+    sound.play();
+    scorePlus();
 }
 
 
 function change2(){
-    a.src="pop3.png"
+    a.src = "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FHMOg9%2FbtrFKHG5RUb%2F0IQZZ5RiooYNYoPHSIRtZk%2Fimg.png";
 }
 
 
 
 
 
-a.addEventListener("mousedown",change1)
-a.addEventListener("mouseup",change2)
-a.addEventListener("touchstart",change1)
-a.addEventListener("touchmove",change2)
+a.addEventListener("mousedown",change1);
+a.addEventListener("mouseup",change2);
+a.addEventListener("touchstart",change1);
+a.addEventListener("touchmove",change2);
+
+
+
+function scorePlus(){
+    j++;
+    score.innerHTML=j;
+}
